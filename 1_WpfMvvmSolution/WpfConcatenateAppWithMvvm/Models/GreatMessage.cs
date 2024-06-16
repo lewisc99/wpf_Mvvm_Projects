78af31c8eb76f83e9c50586bcaf0e68ca4c937a9
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-06-16 18:50:39 -0300

    Wpf ConcatenateAppWith Mvvm finished it

---
+++ 1_WpfMvvmSolution/WpfConcatenateAppWithMvvm/Models/GreatMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WpfConcatenateAppWithMvvm.Models
+{
+    public class GreatMessage
+    {
+        public string Input1 { get; set; }
+        public string Input2 { get; set; }
+        public string ConcatenatedMessage { get; set; }
+    }
+}
---
+++ 1_WpfMvvmSolution/WpfConcatenateAppWithMvvm/ViewModel/GreatMessageViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using WpfConcatenateAppWithMvvm.Commands;
+using WpfConcatenateAppWithMvvm.Models;
+
+namespace WpfConcatenateAppWithMvvm.ViewModel
+{
+    public class GreatMessageViewModel : INotifyPropertyChanged
+    {
+        private GreatMessage _model;
+
+        public string Input1
+        {
+            get { return _model.Input1; }
+            set
+            {
+                _model.Input1 = value;
+                OnPropertyChanged(nameof(Input1));
+                OnPropertyChanged(nameof(CanGenerateMessage)); // Update button enable/disable state
+            }
+        }
+
+        public string Input2
+        {
+            get { return _model.Input2; }
+            set
+            {
+                _model.Input2 = value;
+                OnPropertyChanged(nameof(Input2));
+                OnPropertyChanged(nameof(CanGenerateMessage)); // Update button enable/disable state
+            }
+        }
+
+        public string ConcatenatedMessage
+        {
+            get { return _model.ConcatenatedMessage; }
+            set { _model.ConcatenatedMessage = value; OnPropertyChanged(nameof(ConcatenatedMessage)); }
+        }
+
+        public bool CanGenerateMessage => !string.IsNullOrEmpty(Input1) && !string.IsNullOrEmpty(Input2);
+
+        public ICommand GenerateMessageCommand { get; }
+
+        public GreatMessageViewModel()
+        {
+            _model = new GreatMessage();
+            GenerateMessageCommand = new RelayCommand(GenerateMessage);
+        }
+
+        private void GenerateMessage()
+        {
+            ConcatenatedMessage = String.Concat("Your great message: ", Input1, " ", Input2);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
