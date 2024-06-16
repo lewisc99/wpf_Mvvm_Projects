f6491b0cbe566b4f78a2858aa313a8cabec63cce
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-06-16 17:55:00 -0300

    another mvvm example click a button change the message

---
+++ 1_WpfMvvmSolution/MVVMExample/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using MVVMExample.Commands;
+using MVVMExample.Model;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace MVVMExample.ViewModel
+{
+    public class MainViewModel : INotifyCollectionChanged
+    {
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        private MessageModel _messageModel;
+
+        public ICommand UpdateMessageCommand { get; }
+
+        public MainViewModel()
+        {
+            _messageModel = new MessageModel { Message = "Hello, World!" };
+            UpdateMessageCommand = new RelayCommand(UpdateMessage);
+        }
+
+        public string Message
+        {
+            get => _messageModel.Message;
+            set
+            {
+                if (_messageModel.Message != value)
+                {
+                    _messageModel.Message = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private void UpdateMessage(object parameter)
+        {
+            Message = "Button Clicked!";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
