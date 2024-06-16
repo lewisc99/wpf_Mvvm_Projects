6a919d0a37ebe0ca374a4d242d22df4d94807a5d
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-06-16 15:58:41 -0300

    added fist mvvm project

---
+++ 1_WpfMvvmSolution/Wpf_Mvvm/Model/Contato.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Mvvm.Model
+{
+    public class Contato : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Esta rotina é chamada cada vez que o valor da propridade 
+        // for definida. Isso vai disparar um evento para notificar 
+        // a WPF via data binding que algo mudou
+        private void OnPropertyChanged(string nomePropriedade)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nomePropriedade));
+        }
+        private string _nome;
+        private string _sobrenome;
+        private string _telefone;
+        private string _email;
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                _nome = value;
+                OnPropertyChanged("Nome");
+            }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+        public string Sobrenome
+        {
+            get { return _sobrenome; }
+            set
+            {
+                _sobrenome = value;
+                OnPropertyChanged("Sobrenome");
+            }
+        }
+        public string Telefone
+        {
+            get { return _telefone; }
+            set
+            {
+                _telefone = value;
+                OnPropertyChanged("Telefone");
+            }
+        }
+    }
+}
---
+++ 1_WpfMvvmSolution/Wpf_Mvvm/ViewModel/ContatoViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf_Mvvm.Model;
+
+namespace Wpf_Mvvm.ViewModel
+{
+    public class ContatoViewModel : ObservableCollection<Contato>
+    {
+        public ContatoViewModel()
+        {
+            PreparaContatoCollection();
+        }
+        private void PreparaContatoCollection()
+        {
+            var Contato1 = new Contato
+            {
+                Nome = "Jose Carlos",
+                Sobrenome = "Macoratti",
+                Email = "macoratti@yahoo.com",
+                Telefone = "012-8789-0200"
+            };
+            Add(Contato1);
+            var Contato2 = new Contato
+            {
+                Nome = "Bruno",
+                Sobrenome = "Silveira",
+                Email = "brunosilv@email.com",
+                Telefone = "021-9987-3458"
+            };
+            Add(Contato2);
+            var Contato3 = new Contato
+            {
+                Nome = "Jonatas",
+                Sobrenome = "Sanches",
+                Email = "jonsanch@email.com",
+                Telefone = "011-8975-57898"
+            };
+            Add(Contato3);
+        }
+    }
+}
